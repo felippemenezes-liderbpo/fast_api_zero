217093eec16edf6a8002805cf280f50af6bac32d
Felippe Menezes <felippe.menezes@liderbpo.com.br>
2025-12-27 22:50:33 -0300

    Improve application with schemas and new tests

--- app/main.py
+++ app/main.py
@@ -1,8 +1,27 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from app.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
-def read_root() -> dict[str, str]:
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
+def read_root():
     return {'message': 'Hello World!'}
+
+
+@app.get('/html', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def read_html():
+    return """
+    <html>
+      <head>
+        <title>Hello World</title>
+      </head>
+      <body>
+        <h1>Hello world's page</h1>
+      </body>
+    </html>
+    """
--- tests/test_main.py
+++ tests/test_main.py
@@ -4,11 +4,20 @@ from fastapi.testclient import TestClient
 
 from app.main import app
 
+client = TestClient(app)
 
-def test_root_must_return_ok_and_hello_world() -> None:
-    client = TestClient(app)
 
+def test_root_must_return_ok_headers_json_and_message_hello_world() -> None:
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
+    assert response.headers['content-type'].startswith('application/json')
     assert response.json() == {'message': 'Hello World!'}
+
+
+def test_html_must_return_ok_headers_html_and_title_hello_world() -> None:
+    response = client.get('/html')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.headers['content-type'].startswith('text/html')
+    assert '<title>Hello World</title>' in response.text
